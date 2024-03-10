cb67a2dadbc81bd956aabc642033520a886bd599
Raazi Muhammed K <raazi6163@gmail.com>
2024-03-10 21:44:58 +0530

    feat: convert into pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,59 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+    registerType: "prompt",
+    includeAssests: ["icons/favicon.ico", "icons/apple-touch-icon.png"],
+    manifest: {
+        name: "Mindful Memos",
+        short_name: "Mindful Memos",
+        description: "A Memos app",
+        icons: [
+            {
+                src: "/icons/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "favicon",
+            },
+            {
+                src: "/icons/android-chrome-144x144.png",
+                sizes: "144x144",
+                type: "image/png",
+                purpose: "any",
+            },
+            {
+                src: "/icons/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "favicon",
+            },
+            {
+                src: "/icons/apple-touch-icon.png",
+                sizes: "180x180",
+                type: "image/png",
+                purpose: "apple touch icon",
+            },
+            {
+                src: "/icons/maskable_icon.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "any maskable",
+            },
+        ],
+        theme_color: "#26265F",
+        background_color: "#CED7E9",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+    },
+};
 
 export default defineConfig({
-    plugins: [react()],
+    // @ts-ignore
+    plugins: [react(), VitePWA(manifestForPlugIn)],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
